94ddbab4f6baf54363ccfaad9bc95dc5a1efc17b
Weby <info@geeq.ch>
2020-11-21 09:02:45 +0100

    Moved "super"

--- src/main/java/dev/glassey/Test.java
+++ src/main/java/dev/glassey/Test.java
@@ -6,13 +6,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Test extends JavaPlugin {
     @Override
     public void onDisable() {
-        Bukkit.getLogger().info("Noooooonnn!!!");
         super.onDisable();
+        Bukkit.getLogger().info("Noooooonnn!!!");
     }
     
     @Override
     public void onEnable() {
-        Bukkit.getLogger().info("Ouiiii!!!");
         super.onEnable();
+        Bukkit.getLogger().info("Ouiiii!!!");
     }
 }
